e839ddb6b48d42a283a452c27678b2038244cdf8
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-01-22 19:51:43 -0500

    speedrun

---
+++ index.js
@@ -0,0 +1,60 @@
+let quizzes = document.getElementById("quizzes");
+let grade = document.getElementById("grade");
+function recalculate() {
+  // get value of first input
+  let quizzesweight = quizzes.querySelector("input").value;
+  quizzesweight < 0 ? (quizzesweight = 0) : quizzesweight;
+  quizzesweight > 100 ? (quizzesweight = 100) : quizzesweight;
+  let quizzesgrades = quizzes.querySelectorAll("input");
+  let total = 0;
+  for (let i = 1; i < quizzesgrades.length; i++) {
+    quizzesgrades[i].value < 0
+      ? (quizzesgrades[i].value = 0)
+      : quizzesgrades[i].value;
+    quizzesgrades[i].value > 100
+      ? (quizzesgrades[i].value = 100)
+      : quizzesgrades[i].value;
+    total += isNaN(parseInt(quizzesgrades[i].value))
+      ? 0
+      : parseInt(quizzesgrades[i].value);
+  }
+  grade.innerText =
+    ((total / ((quizzesgrades.length - 1) * 100)) * 100).toFixed(2) + "%";
+}
+
+function bindInputs() {
+  let quizzesgrades = quizzes.querySelectorAll("input");
+  for (let i = 0; i < quizzesgrades.length; i++) {
+    quizzesgrades[i].onchange = function () {
+      console.warn(quizzesgrades[i].value);
+      recalculate();
+    };
+  }
+}
+bindInputs();
+
+let addquiz = document.getElementById("addquiz");
+addquiz.addEventListener("click", function () {
+  // get last div with class "week"
+  let quizzesgrades = quizzes.querySelectorAll("input");
+
+  let lastquiz = quizzes.getElementsByClassName("week");
+  lastquiz[lastquiz.length - 1].insertAdjacentHTML(
+    "afterend",
+    `<div class="week" style="position: relative"><input type="number" id="input" placeholder="Week ${quizzesgrades.length}"/><button class="x button buttont" style="position: absolute; top: -.25rem; right: -.25rem;"><b>+</b></button></div>`
+  );
+  closingButtons();
+  recalculate();
+  bindInputs();
+});
+
+function closingButtons() {
+  let xbuttons = document.querySelectorAll("button.x.button.buttont");
+  for (let i = 0; i < xbuttons.length; i++) {
+    xbuttons[i].addEventListener("click", function () {
+      this.parentNode.remove();
+      recalculate();
+      bindInputs();
+    });
+  }
+}
